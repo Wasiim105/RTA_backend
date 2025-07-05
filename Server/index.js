6323dd77fc69ad08f8836c70a068740d29257832
Wasim Ansari <99980623+Wasiim105@users.noreply.github.com>
2025-07-05 20:55:44 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,10 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  credentials: true
+}));
 
 // Use Helmet middleware for basic security headers
 app.use(helmet());
